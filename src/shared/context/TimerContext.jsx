cb916dbbf775c9037de574d760654bb6531bbcf1
aymen-igr <aymenigri@gmail.com>
2025-09-30 11:21:39 +0100

    adding some modifs on the shared structure

--- src/shared/context/TimerContext.jsx
+++ src/shared/context/TimerContext.jsx
@@ -1,25 +1,3 @@
-import { createContext , useEffect, useState} from "react";
+import { createContext} from "react";
 
 export const TimerContext = createContext();
-
-export function TimerProvider({ children }) {
-
-    const [time, setTime] = useState(60);
-    const [isRunning, setIsRunning] = useState(true);
-
-    useEffect(()=>{
-            let interval = null;
-            if(isRunning && time>0){
-                interval = setInterval(()=>{
-                    setTime(time => time - 1);
-                },1000)
-            }
-            return  () => clearInterval(interval);
-        },[isRunning, time])
-
-    return (
-        <TimerContext.Provider value={{ time, setTime, isRunning, setIsRunning }}>
-            {children}
-        </TimerContext.Provider>
-    );
-}
---
+++ src/shared/providers/TimerProvider.jsx
@@ -0,0 +1,23 @@
+import {useEffect, useState} from "react";
+
+export function TimerProvider({ children }) {
+
+    const [time, setTime] = useState(60);
+    const [isRunning, setIsRunning] = useState(true);
+
+    useEffect(()=>{
+            let interval = null;
+            if(isRunning && time>0){
+                interval = setInterval(()=>{
+                    setTime(time => time - 1);
+                },1000)
+            }
+            return  () => clearInterval(interval);
+        },[isRunning, time])
+
+    return (
+        <TimerContext.Provider value={{ time, setTime, isRunning, setIsRunning }}>
+            {children}
+        </TimerContext.Provider>
+    );
+}
