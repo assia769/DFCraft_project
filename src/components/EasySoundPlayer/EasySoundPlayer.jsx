248c9e4cad3763c51bdb5531a5db5e36d5cd3131
Aymen Igri <aymenigri@gmail.com>
2026-02-01 02:42:38 +0100

    Changing the style of the home page

---
+++ src/components/EasySoundPlayer/EasySoundPlayer.jsx
@@ -0,0 +1,5 @@
+export default function EasySoundPlayer() {
+  return (<div className="bg-lightElements dark:bg-darkElements px-4 py-2 mt-5 mx-14 rounded-full">
+    sound player
+  </div>)
+}
---
+++ src/components/EasyTasks/EasyTasks.jsx
@@ -0,0 +1,12 @@
+export default function EasyTasks() {
+  return (<div className="bg-lightElements dark:bg-darkElements px-4 py-2 mt-5 mx-14 rounded-full flex flex-row justify-between items-center">
+    <p>
+      Total completed tasks:
+    </p>
+    <div>
+      <p>
+        1/10
+      </p>
+    </div>
+  </div>);
+}
