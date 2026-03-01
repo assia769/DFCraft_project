150bad8a1e246c7aa09c7fe898085a3acd2d62bc
VisualVibe <Visual.vibe1999@gmail.com>
2026-03-01 13:26:58 +0000

    fixer quelque bugs

--- src/background/blockerWorker.js
+++ src/background/blockerWorker.js
@@ -9,6 +9,7 @@ import { browserAPI } from "../shared/utils/browserAPI";
 async function getUrls() {
     try {
         const result = await browserAPI.storage.local.get('urls');
+        console.log('result', result)
         return result.urls || [];
     } catch (err) {
         console.error('Erreur lors de la récupération des URLs :', err);
@@ -41,22 +42,17 @@ async function blockAccess(result , tab  ) {
 
 // Bloquer ou activer le son sur les onglets
 async function blockSound( result) {
-
-
     result.forEach(async (element) => {
-        if (element.soundBlocked) {
-
+        if (element.sowndBlocked) {
             const tabs = await browserAPI.tabs.query({});
+            console.log('tabs', tabs)
             for (const tab of tabs) {
                 if (tab.url && tab.url.includes(element.url)) {
-                    await browserAPI.tabs.update(tab.id, { muted: element.soundBlocked });
+                    await browserAPI.tabs.update(tab.id, { muted: true });
                 }
             }
         }
     });
-
-
-
 }
 
 
@@ -71,8 +67,9 @@ async function blockSound( result) {
 export function blockWorker() {
            browserAPI.tabs.onUpdated.addListener(async (tabId, changeInfo, tab)=> {
                 try {
+                    console.log("Listner onUpdated")
                     const result = await getUrls()
-                     await blockAccess(result ,  tab )
+                     await blockAccess(result ,tab)
                      await blockSound(result)
                 } catch (error) {
                     console.error("Erreur dans le blocage :", error);
@@ -82,30 +79,30 @@ export function blockWorker() {
 
 
 
-export function signalReceiverBlocker() {
-    browserAPI.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-        try {
-            if (message.type === "BLOC") {
-                const result = await getUrls
-                result.forEach(async (element) => {
-                    if (message.sownd) {
-                        await blockSound(element);
-                    }
-                    if (message.acces) {
-                        await blockAccess(element);
+// export function signalReceiverBlocker() {
+//     browserAPI.runtime.onMessage.addListener(async (message, sendResponse) => {
+//         try {
+//             if (message.type === "BLOC") {
+//                 const result = await getUrls
+//                 result.forEach(async (element) => {
+//                     if (message.sownd) {
+//                         await blockSound(element);
+//                     }
+//                     if (message.acces) {
+//                         await blockAccess(element);
 
-                    }
-                });
+//                     }
+//                 });
 
 
-                // On peut répondre si on veut
-                sendResponse({ status: "ok", message: "Timer lancé !" });
-            }
-        } catch (error) {
-            console.error("Erreur dans le blocage :", error);
-            sendResponse({ status: "error", message: err.message });
-        }
+//                 // On peut répondre si on veut
+//                 sendResponse({ status: "ok", message: "Timer lancé !" });
+//             }
+//         } catch (error) {
+//             console.error("Erreur dans le blocage :", error);
+//             sendResponse({ status: "error", message: err.message });
+//         }
 
-        return true;
-    });
-}
+//         return true;
+//     });
+// }
